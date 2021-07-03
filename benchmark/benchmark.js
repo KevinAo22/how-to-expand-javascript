741cbc642fcc387b89e9b6987d0c53f344ac7bb5
Ao Zhang <kevinao@kevinao.com>
2021-07-04 00:36:10 +0800

    refactor(benchmark): add several data sets for benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -13,13 +13,17 @@ const generateRandomUint8Array = (length) => {
 
 const key = '000102030405060708090a0b0c0d0e0f';
 const iv = '101112131415161718191a1b1c1d1e1f';
-const source = Buffer.from(generateRandomUint8Array(1024)).toString('base64');
-const encrypted = native.encryptInAes(source, key, iv);
+const smallSource = Buffer.from(generateRandomUint8Array(1024)).toString('base64');
+const smallEncrypted = native.encryptInAes(smallSource, key, iv);
+const mediumSource = Buffer.from(generateRandomUint8Array(256 * 1024)).toString('base64');
+const mediumEncrypted = native.encryptInAes(mediumSource, key, iv);
+const bigSource = Buffer.from(generateRandomUint8Array(64 * 1024 * 1024)).toString('base64');
+const bigEncrypted = native.encryptInAes(bigSource, key, iv);
 
-const aesDecryptSuite = new Benchmark.Suite('AES decrypt: ');
-aesDecryptSuite
+const smallAesDecryptSuite = new Benchmark.Suite('AES decrypt for small data: ');
+smallAesDecryptSuite
   .add('Native', () => {
-    native.decryptInAes(encrypted, key, iv);
+    native.decryptInAes(smallEncrypted, key, iv);
   })
   .on('start', (event) => {
     console.log(event.currentTarget.name);
@@ -31,4 +35,36 @@ aesDecryptSuite
     console.log(` Fastest is ${event.currentTarget.filter('fastest').map('name')}`);
   });
 
-aesDecryptSuite.run();
+const mediumAesDecryptSuite = new Benchmark.Suite('AES decrypt for medium data: ');
+mediumAesDecryptSuite
+  .add('Native', () => {
+    native.decryptInAes(mediumEncrypted, key, iv);
+  })
+  .on('start', (event) => {
+    console.log(event.currentTarget.name);
+  })
+  .on('cycle', (event) => {
+    console.log(`   ${String(event.target)}`);
+  })
+  .on('complete', (event) => {
+    console.log(` Fastest is ${event.currentTarget.filter('fastest').map('name')}`);
+  });
+
+const bigAesDecryptSuite = new Benchmark.Suite('AES decrypt for big data: ');
+bigAesDecryptSuite
+  .add('Native', () => {
+    native.decryptInAes(bigEncrypted, key, iv);
+  })
+  .on('start', (event) => {
+    console.log(event.currentTarget.name);
+  })
+  .on('cycle', (event) => {
+    console.log(`   ${String(event.target)}`);
+  })
+  .on('complete', (event) => {
+    console.log(` Fastest is ${event.currentTarget.filter('fastest').map('name')}`);
+  });
+
+smallAesDecryptSuite.run();
+mediumAesDecryptSuite.run();
+bigAesDecryptSuite.run();
