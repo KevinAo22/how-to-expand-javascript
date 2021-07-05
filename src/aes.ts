e7596205e2a9ab83fc00a979f6dd7d7a3adff3b6
Ao Zhang <kevinao@kevinao.com>
2021-07-05 23:16:54 +0800

    refactor(native): rename parameters

--- src/aes.ts
+++ src/aes.ts
@@ -1,15 +1,15 @@
 import { AES, enc, mode, pad } from 'crypto-js';
 
-export function encryptInAes(source: string, keyStr: string, ivString: string): string {
+export function encryptInAes(source: string, keyStr: string, ivStr: string): string {
   const key = enc.Hex.parse(keyStr);
-  const iv = enc.Hex.parse(ivString);
+  const iv = enc.Hex.parse(ivStr);
   const encrypted = AES.encrypt(enc.Base64.parse(source), key, { iv, mode: mode.CBC, padding: pad.Pkcs7 });
   return encrypted.ciphertext.toString(enc.Base64);
 }
 
-export function decryptInAes(source: string, keyStr: string, ivString: string): string {
+export function decryptInAes(source: string, keyStr: string, ivStr: string): string {
   const key = enc.Hex.parse(keyStr);
-  const iv = enc.Hex.parse(ivString);
+  const iv = enc.Hex.parse(ivStr);
   const decrypted = AES.decrypt(source, key, { iv, mode: mode.CBC, padding: pad.Pkcs7 });
   return decrypted.toString(enc.Base64);
 }
