c0608985acd105db00f6cc169c98bef02fcb2469
Ao Zhang <kevinao@kevinao.com>
2021-07-14 12:24:12 +0800

    refactor(benchmark): add more data set for benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,4 +1,5 @@
 const Benchmark = require('benchmark');
+const os = require('os');
 const nativeAES = require('../native/dist/aes');
 const wasmAES = require('../wasm/build/nodejs/wasm');
 const addonAES = require('../addon/addon.darwin-x64.node');
@@ -13,84 +14,48 @@ const generateRandomUint8Array = (length) => {
   return uint8Array;
 };
 
+const bytesToSize = (bytes) => {
+  if (bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.min(parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10), sizes.length - 1);
+    return `${(bytes / 1024 ** i).toFixed(i ? 1 : 0)}${sizes[i]}`;
+  }
+  return 'n/a';
+};
+
 const keyStr = '000102030405060708090a0b0c0d0e0f';
 const ivStr = '101112131415161718191a1b1c1d1e1f';
 const keyBuffer = Buffer.from(keyStr, 'hex');
 const ivBuffer = Buffer.from(ivStr, 'hex');
 
-const smallSource = generateRandomUint8Array(1024);
-const smallEncrypted = addonAES.encryptInAes(smallSource, keyBuffer, ivBuffer);
-const smallEncryptedStr = Buffer.from(smallEncrypted).toString('base64');
-const mediumSource = generateRandomUint8Array(256 * 1024);
-const mediumEncrypted = addonAES.encryptInAes(mediumSource, keyBuffer, ivBuffer);
-const mediumEncryptedStr = Buffer.from(mediumEncrypted).toString('base64');
-const bigSource = generateRandomUint8Array(1024 * 1024);
-const bigEncrypted = addonAES.encryptInAes(bigSource, keyBuffer, ivBuffer);
-const bigEncryptedStr = Buffer.from(bigEncrypted).toString('base64');
+console.log('CPU Arch:', os.arch());
+console.log('CPU Info:', os.cpus()[0].model);
+console.log(`Memory Info: ${Math.floor(os.totalmem() / 1024 / 1024 / 1024)}GB`);
 
-const smallAesDecryptSuite = new Benchmark.Suite('AES decrypt for 1KB data: ');
-smallAesDecryptSuite
-  .add('Native', () => {
-    nativeAES.decryptInAes(smallEncryptedStr, keyStr, ivStr);
-  })
-  .add('WebAssembly', () => {
-    wasmAES.decrypt_in_aes(smallEncrypted, keyBuffer, ivBuffer);
-  })
-  .add('N-API Addon', () => {
-    addonAES.decryptInAes(smallEncrypted, keyBuffer, ivBuffer);
-  })
-  .on('start', (event) => {
-    console.log(event.currentTarget.name);
-  })
-  .on('cycle', (event) => {
-    console.log(`    ${event.target}`);
-  })
-  .on('complete', (event) => {
-    console.log(`  Fastest is ${event.currentTarget.filter('fastest').map('name')}`);
-  });
-
-const mediumAesDecryptSuite = new Benchmark.Suite('AES decrypt for 256KB data: ');
-mediumAesDecryptSuite
-  .add('Native', () => {
-    nativeAES.decryptInAes(mediumEncryptedStr, keyStr, ivStr);
-  })
-  .add('WebAssembly', () => {
-    wasmAES.decrypt_in_aes(mediumEncrypted, keyBuffer, ivBuffer);
-  })
-  .add('N-API Addon', () => {
-    addonAES.decryptInAes(mediumEncrypted, keyBuffer, ivBuffer);
-  })
-  .on('start', (event) => {
-    console.log(event.currentTarget.name);
-  })
-  .on('cycle', (event) => {
-    console.log(`    ${event.target}`);
-  })
-  .on('complete', (event) => {
-    console.log(`  Fastest is ${event.currentTarget.filter('fastest').map('name')}`);
-  });
-
-const bigAesDecryptSuite = new Benchmark.Suite('AES decrypt for 1MB data: ');
-bigAesDecryptSuite
-  .add('Native', () => {
-    nativeAES.decryptInAes(bigEncryptedStr, keyStr, ivStr);
-  })
-  .add('WebAssembly', () => {
-    wasmAES.decrypt_in_aes(bigEncrypted, keyBuffer, ivBuffer);
-  })
-  .add('N-API Addon', () => {
-    addonAES.decryptInAes(bigEncrypted, keyBuffer, ivBuffer);
-  })
-  .on('start', (event) => {
-    console.log(event.currentTarget.name);
-  })
-  .on('cycle', (event) => {
-    console.log(`    ${event.target}`);
-  })
-  .on('complete', (event) => {
-    console.log(`  Fastest is ${event.currentTarget.filter('fastest').map('name')}`);
-  });
-
-smallAesDecryptSuite.run();
-mediumAesDecryptSuite.run();
-bigAesDecryptSuite.run();
+for (let index = 0; index < 8; index += 1) {
+  const dataSizeRatio = Math.max(1, index * 256);
+  const source = generateRandomUint8Array(1024 * dataSizeRatio);
+  const encrypted = addonAES.encryptInAes(source, keyBuffer, ivBuffer);
+  const encryptedStr = Buffer.from(source).toString('base64');
+  const decryptSuite = new Benchmark.Suite(`AES decrypt for ${bytesToSize(1024 * dataSizeRatio)} data: `);
+  decryptSuite
+    .add('Native JavaScript', () => {
+      nativeAES.decryptInAes(encryptedStr, keyStr, ivStr);
+    })
+    .add('WebAssembly', () => {
+      wasmAES.decrypt_in_aes(encrypted, keyBuffer, ivBuffer);
+    })
+    .add('N-API Addon', () => {
+      addonAES.decryptInAes(encrypted, keyBuffer, ivBuffer);
+    })
+    .on('start', (event) => {
+      console.log(event.currentTarget.name);
+    })
+    .on('cycle', (event) => {
+      console.log(`    ${event.target}`);
+    })
+    .on('complete', (event) => {
+      console.log(`  Fastest is ${event.currentTarget.filter('fastest').map('name')}`);
+    });
+  decryptSuite.run();
+}
